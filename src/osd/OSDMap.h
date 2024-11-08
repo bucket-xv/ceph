5a86419d7d6f0b4812f5bbfa38790e28bffaf4f1
Chenhao Xu <xv_chen_hao@163.com>
2024-11-08 19:53:47 +0800

    Fix

--- src/osd/OSDMap.h
+++ src/osd/OSDMap.h
@@ -1491,6 +1491,14 @@ public:
     Incremental *pending_inc,
     OSDMap& tmp_osd_map,
     const std::optional<rb_policy>& rbp = std::nullopt) const;
+  
+  int OSDMap::balance_ec_primaries(
+    CephContext *cct,
+    int64_t pid,
+    OSDMap::Incremental *pending_inc,
+    OSDMap &tmp_osd_map,
+    std::map<int, uint64_t> &bytes_used,
+    const std::optional<rb_policy> &rbp) const;
 
   void rm_all_upmap_prims(CephContext *cct, Incremental *pending_inc, uint64_t pid);
 
